b1664ce3fc583e253ded8fe4dee42a62e4a0d7fa
jiada <jiada@JD>
2023-05-23 10:18:23 +0200

    Clase conectar

--- src/Control/Conexion.java
+++ src/Control/Conexion.java
@@ -1,21 +1,23 @@
-package Clases;
+package Control;
 
 import com.sun.jdi.connect.spi.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Logger;
+import javax.persistence.*;
 
 /**
  *
  * @author jiada
  */
-public class Conectar {
+public class Conexion {
+
     Connection conectar = null;
     
     public Connection conexion(){
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            java.sql.Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/proyectofinal","jiada","1234");
+            conectar = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/proyectofinal","jiada","1234");
         } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(ex.getMessage());
         } 
