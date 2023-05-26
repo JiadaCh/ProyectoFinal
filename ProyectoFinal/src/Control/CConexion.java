5efb9fa856758fbb64821ac0997c58b07baeec17
Jiada <124351114+JiadaCh@users.noreply.github.com>
2023-05-26 09:25:13 +0200

    Add files via upload
    funciones

---
+++ ProyectoFinal/src/Control/CConexion.java
@@ -0,0 +1,52 @@
+package Control;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+import java.sql.*;
+
+/**
+ *
+ * @author jiada
+ */
+public class CConexion{
+
+    final String url = "jdbc:mysql://localhost:3306/proyectofinal";
+    String user = "jiada";
+    String password = "1234";
+    final String driver = "com.mysql.cj.jdbc.Driver";
+    private Connection conexion = null;
+
+    public CConexion() {
+
+    }
+
+    public Connection conectar() {
+
+        try {
+            Class.forName(driver);
+            conexion = (Connection) DriverManager.getConnection(url, user, password);
+            return getConexion();
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(ex.getMessage());
+            return null;
+        }
+
+        
+    }
+
+    public void desconectar() {
+        try {
+            getConexion().close();
+        } catch (SQLException ex) {
+            Logger.getLogger(ex.getMessage());
+        }
+    }
+
+    public Connection getConexion() {
+        return conexion;
+    }
+
+    
+
+}
---
+++ ProyectoFinal/src/Proyecto/dialogo.java
@@ -0,0 +1,14 @@
+
+package Proyecto;
+
+import Ventanas.Login;
+
+public class dialogo {
+
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.setVisible(true);
+        
+    }
+    
+}
